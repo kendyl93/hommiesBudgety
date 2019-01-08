ee4d645bf30856a9670e531759531ab2aff083d4
Pawel Stanecki <pawel.stanecki@sandstream.pl>
2019-01-08 21:53:37 +0100

    Added sparkline library

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import logo from '../../logo.svg';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 const Home = props => {
   return (
     <header className="App-header">
+      <Sparklines data={[5, 10, 5, 20]}>
+        <SparklinesLine color="blue" />
+      </Sparklines>
       <img src={logo} className="App-logo" alt="logo" />
       <h1>Home Page</h1>
       <h3>Todo:</h3>
-      <p>
+      <div>
         <ul>
           <li>Add sparklines and some temporary data</li>
           <li>Add Avatar component</li>
           <li>Fix nav at mobile devices</li>
         </ul>
-      </p>
+      </div>
     </header>
   );
 };
