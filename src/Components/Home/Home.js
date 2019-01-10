4c2f5549cb21be9c6670ebd9f73f18788b1da0b2
Pawel Stanecki <pawel.stanecki@sandstream.pl>
2019-01-10 21:40:21 +0100

    Improove the home page view

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,27 +1,45 @@
 import React from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { Sparklines, SparklinesBars, SparklinesLine } from 'react-sparklines';
 import styled from 'styled-components';
+import { Row, Col } from 'react-bootstrap';
 
 const SparkLineBox = styled.div`
-  padding-top: 16px;
   border: solid 1px #61dafb;
   border-radius: 3px;
+`;
+const HeaderBox = styled.h6`
+  padding: 8px 0 0 8px;
 `;
 
 const Home = props => {
   return (
     <header className="App-header">
-      <SparkLineBox>
-        <Sparklines data={[5, 102, 53, 205, 5, 130, 56, 204, 51, 190, 51, 90]}>
-          <SparklinesLine color="#61DAFB" />
-        </Sparklines>
-      </SparkLineBox>
+      <Row>
+        <Col sm={6}>
+          <SparkLineBox>
+            <HeaderBox>Overall</HeaderBox>
+            <Sparklines
+              data={[5, 102, 53, 205, 5, 130, 56, 204, 51, 190, 51, 90]}
+            >
+              <SparklinesLine color="#61DAFB" />
+            </Sparklines>
+          </SparkLineBox>
+        </Col>
+        <Col sm={6}>
+          <SparkLineBox>
+            <HeaderBox>Outcome</HeaderBox>
+            <Sparklines data={[50, 70, 53, 50, 50, 70, 56, 45, 51, 90, 51, 90]}>
+              <SparklinesBars
+                style={{ stroke: 'white', strokeWidth: '1', fill: '#40c0f5' }}
+              />
+            </Sparklines>
+          </SparkLineBox>
+        </Col>
+      </Row>
       <h1>Home Page</h1>
       <h3>Todo:</h3>
       <div>
         <ul>
-          <li>Add sparklines and some temporary data</li>
-          <li>Add Avatar component</li>
           <li>Fix nav at mobile devices</li>
         </ul>
       </div>
